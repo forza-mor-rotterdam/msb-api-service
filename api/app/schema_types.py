b242f7815a2aa1e6961ffbd1ebef34edf1cfd490
Jorrit Strikwerda <jippiejorrit@gmail.com>
2024-04-08 19:15:04 +0200

    Rename to_json

--- api/app/schema_types.py
+++ api/app/schema_types.py
@@ -15,7 +15,7 @@ class QuestionAnswerPair(BaseModel):
     question: str
     answers: List[str]
 
-    def json(self):
+    def to_json(self):
         return {
             "question": self.question if self.question else "",
             "answers": self.answers if self.answers else [],
@@ -73,7 +73,7 @@ class MorMeldingAanmakenRequest(BaseModel):
         json_encoders = {
             datetime: lambda v: v.isoformat(),
             QuestionAnswerPair: lambda v: (
-                v.json() if isinstance(v, QuestionAnswerPair) else v
+                v.to_json() if isinstance(v, QuestionAnswerPair) else v
             ),
         }
         now = datetime.now(pytz.timezone("Europe/Amsterdam")).isoformat()
